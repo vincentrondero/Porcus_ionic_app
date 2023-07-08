ade799751d75e20e145eb853e95719914fb3d7a8
vincentrondero <vincentpatrickrondero@gmail.com>
2023-07-08 16:42:47 +0800

    overlay_action_fixed_css_not

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 
+type Task = {
+  name: string;
+  done: boolean;
+  deadline: Date;
+};
+
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  tasks: Task[] = [];
+  newTask!: string;
+  doneTasks: Task[] = [];
+  newTaskDeadline!: string;
+  
 
   constructor() {}
 
 
   div1:boolean=true;
   div2:boolean=false;
+  showOverlayContent: boolean = false;
 
 
   div1Function(){
@@ -29,7 +42,35 @@ export class Tab3Page {
   toggleCheckbox() {
     this.isChecked = !this.isChecked;
   }
+  showOverlay() {
+    this.showOverlayContent = true;
+  }
 
+  hideOverlay() {
+    this.showOverlayContent = false;
+  }
+
+  addTask() {
+    if (this.newTask && this.newTaskDeadline) {
+      const task: Task = {
+        name: this.newTask,
+        done: false,
+        deadline: new Date(this.newTaskDeadline)
+      };
+      this.tasks.push(task);
+      this.newTask = '';
+      this.newTaskDeadline = '';
+    }
+  }
+  toggleTaskDone(task: Task) {
+    task.done = true;
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.doneTasks.push(task);
+    }
+  }
+  
 }
 
 
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google: any;
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
@@ -10,6 +17,28 @@ export class Tab4Page implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.loadGoogleMapsScript(() => {
+      this.initMap();
+    });
   }
-
+  
+  loadGoogleMapsScript(callback: () => void) {
+    if (typeof google === 'undefined') {
+      window.initMap = () => {
+        callback();
+      };
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAR4_X0H35IUO0gZc63dJk6RmWiteYX8FY=initMap`;
+      document.body.appendChild(script);
+    } else {
+      callback();
+    }
+  }
+  
+  initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
 }
