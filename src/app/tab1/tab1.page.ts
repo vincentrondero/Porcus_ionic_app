56e4a962c1719de15995b6f88ee668e2981ee495
vincentrondero <vincentpatrickrondero@gmail.com>
2023-07-09 15:44:04 +0800

    authenticatio_firebase

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,26 +14,14 @@ export class Tab1Page implements OnInit {
 
   constructor(private weatherService: WeatherService) {}
 
-  ngOnInit() {
-    //this.weatherData$ = this.weatherService.getWeather(this.locationKey);
-
-    //this.weatherData$.subscribe(
-     // (weatherData: any[]) => {
-       // console.log('Weather Data:', weatherData);
-        // Your other code logic here
-     // },
-     // (error: any) => {
-       // console.log('Error:', error);
-     // }
-   // );
-  }
+  ngOnInit() {}
 
   searchLocation() {
     this.weatherService.getLocationKey(this.location).subscribe(
       (response: any[]) => {
         // Assuming the response is an array of location objects,
         // you can extract the location key from the first item in the array
-        if (response?.length > 0) {
+        if (response.length > 0) {
           this.locationKey = response[0].Key;
           this.getWeatherData();
         } else {
@@ -52,29 +40,13 @@ export class Tab1Page implements OnInit {
     this.weatherData$ = this.weatherService.getWeather(this.locationKey);
   }
   getWeatherIcon(condition: string): string {
-    const defaultIcon = '/assets/H.png'; // Default icon if condition is not found in the mapping
-  
-    if (!condition) {
-      console.log('Condition is undefined or empty');
-      return defaultIcon;
-    }
-  
-    const conditionKey = condition.trim(); // Remove leading/trailing spaces
-  
-    console.log('Condition:', conditionKey);
-    console.log('Mapped Icon:', this.weatherIconMapping[conditionKey]);
-  
-    return this.weatherIconMapping[conditionKey] || defaultIcon;
+    const defaultIcon = 'default-icon'; // Default icon if condition is not found in the mapping
+    return this.weatherIconMapping[condition.toLowerCase()] || defaultIcon;
   }
-   
   weatherIconMapping: { [key: string]: string } = {
-    'Sunny': '/assets/S.png',
-    'Cloudy': '/assets/C.png',
-    'Rainy': '/assets/R.png',
-    'Partly sunny': '/assets/PS.png',
-    'Mostly cloudy': '/assets/MC.png',
-    'Clouds and sun': '/assets/MC.png',
-    'Mostly sunny': '/assets/MS.png',
+    'sunny': 'sunny-icon',
+    'cloudy': 'cloudy-icon',
+    'rainy': 'rainy-icon',
     // Add more mappings for different weather conditions
   };
 }
